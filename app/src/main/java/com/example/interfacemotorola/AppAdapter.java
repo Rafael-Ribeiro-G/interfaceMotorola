ebcb9bfb864b074ad08b280ebd7defe0896c04dc
Rafael Ribeiro Goncalves <r.ribeirogoncalves07@gmail.com>
2026-08-18 23:19:12 -0300

    Versão 1.01 da Interface do Motorola

---
+++ app/src/main/java/com/example/interfacemotorola/AppAdapter.java
@@ -0,0 +1,74 @@
+package com.example.interfacemotorola;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//Essa public class tá criada com o extends para poder obedecer, as regras impostas pelo "RecyclerView", além disso, também está sendo usado o "ViewHolder", para mostrar os ícones e nomes dos aplicativos da maneira que foi esquematizada, além de manter dentro do nosso layout.
+public class AppAdapter extends RecyclerView.Adapter <AppAdapter.ViewHolder> {
+    //Esse comando vai servir para chamar a lista de todos os aplicativos e guarda-lós até que eles sejam apresentados na tela.
+    List<AppInfo> appsList;
+
+    //Essa função vai servir para informar para o Android quando a lista acaba.
+    @Override
+    public int getItemCount() {
+        return appsList.size();
+    }
+
+    //Essa função serve para criar a "moldura" para os aplicativos.
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        //Esse comando é responsável por transformar o texto em objetos reais, então no caso, ele absorve o texto do arquivo item_app.xml e transforma em objetos reais que podem ser exibidos na tela do celular.
+        LayoutInflater inflater= LayoutInflater.from(parent.getContext());
+
+        //Esse comando vai ser responsável por transformar o meu arquivo XML em uma view que pode ser acessada pelo ViewHolderç
+        View view = inflater.inflate(R.layout.item_app, parent, false);
+
+        //Este comando devolve a view dentro do ViewHolder.
+        return new ViewHolder(view);
+    }
+
+    //Essa função vai ser a responsável por apresentar algum item na tela toda vez que for chamado pelo código, no caso ele entrega duas coisas, sendo elas: moldura (ViewHolder) e position (O número do aplicativo).
+    @Override
+    public void onBindViewHolder (ViewHolder Holder, int position){
+        //Pega os dados atuais do aplicativo que está aparecendo na lista
+        AppInfo app = appsList.get(position);
+
+        //Adiciona o nome do aplicativo no TextView
+        Holder.nomeApp.setText(app.label);
+
+        //Adiciona o ícone do aplicativo no ImageView (Utilizamos o Drawable por se tratar de um fator que é desenhavel)
+        Holder.iconeApp.setImageDrawable(app.icon);
+    }
+
+    //Esse construtor vai servir para receber a lista de aplicativos que vão ser mostrados, no caso ele chama todos os aplicativos que estão dentro da lista, que automaticamente são todos os palicativos instalados dentro do celular.
+    public AppAdapter(List<AppInfo>appsList) {
+        this.appsList = appsList;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        //Essas variáveis vão ser responsáveis por pedir o ícone e nome do aplicativo
+        ImageView iconeApp;
+        TextView nomeApp;
+
+        public ViewHolder (View itemView) {
+            //O itemView é a parte "sólida"/"consolidada", ele é o que mantém o ícone e o nome do aplicativo juntos, então quando o Android chamar por determinado aplicativo específico ele vai ser a ferramenta que vai fazer eles aparecem juntos.
+
+            //É o comando que chama o construtor e se refere a classe pai, que nesse caso seria o RecyclerView.
+            super(itemView);
+
+            //Esses dois comandos estão sendo utilizados para chamar o ícone e o nome do aplicativo pelo ID deles, fazendo assim com que o itemView possa visualizar e continuar o processo de impressão deles na tela.
+            iconeApp = itemView.findViewById(R.id.iconeApp);
+            nomeApp = itemView.findViewById(R.id.nomeApp);
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/interfacemotorola/MainActivity.java
+++ app/src/main/java/com/example/interfacemotorola/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.interfacemotorola;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +10,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //O primeiro passo é criar todas as variáveis que vamos precisar chamar futuramente.
+    RecyclerView recyclerView; //É a estante do nosso XML
+    List<AppInfo> appsList; //É a lista onde vai estar presente todos os aplicativos instalados no celular.
+    AppAdapter adapter; //É o responsável por estampar o ícone e nome do aplicativo.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Essa função vai funcionar
+        recyclerView = findViewById(R.id.appList);
+
+        //Essa função vai ter a função de
+        appsList = new ArrayList<>();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //A função abaixo está sendo utilizada para fazer a buscagem/pesquisa dos aplicativos.
+        pegarApps();
+
     }
+
+        //A função abaixo vai ter a responsabilidade de buscar os aplicativos.
+        private void pegarApps() {
+            //Busca todos os aplicativos que tenham função de "ACTION_MAIN", no caso seria a página inicial so aplicativo, quando abre é a primeira coisa que aparece na tela.
+            Intent intent = new Intent(Intent.ACTION_MAIN, null);
+
+            //É quem vai fazer o pedido do aplicativo para aparecer na interface e na lista de ícones.
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            //No comando abaixo vou chamar o get.packageManager para me entregar a lista de aplicativos que ele achou com aquele filtro montado anteriormente.
+            List<ResolveInfo> appsBrutos = getPackageManager().queryIntentActivities(intent, 0);
+
+            //Agora para melhorar o filtro vamos fazer um Loop (for), ele vai criar uma ficha para cada aplicativo.
+            for (ResolveInfo info: appsBrutos) {
+                //Extrair o nome (label)
+                String label = info.loadLabel(getPackageManager()).toString();
+                    String packageName = info.activityInfo.packageName; //Após o "=" a função do comando vai ser procurar pelo endereço único que será usado para encontrar o aplicativo.
+                    Drawable icon = info.loadIcon(getPackageManager()); //Carrega o ícone e envia ele para o packageManager.
+                    appsList.add(new AppInfo(label, packageName, icon));
+            }
+
+                //Agora irei avisar ao Adapter que a lista já está cheia.
+                adapter = new AppAdapter(appsList); //Avisa ao AppAdapter que a lista já está cheia.
+                recyclerView.setAdapter(adapter); //Avisa ao recyclerView que quando for necessário mostrar um ícone é preciso enviar para o adapter.
+        }
 }
